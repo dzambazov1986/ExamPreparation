60350584483de705837a382ca432f70cd1c362b7
Dimityr <dzambazov1986@gmail.com>
2024-02-26 20:03:17 +0200

    Add project files.

---
+++ TestApp.Tests/CenterPointTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class CenterPointTests
+{
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsCloser_ShouldReturnFirstPoint()
+    {
+      //Arrange
+        double x1 = 1;
+        double y1 = 1;
+        double x2 = 2;
+        double y2 = 2;
+
+        string expected = "(1, 1)";
+
+       //Act
+        string result = CenterPoint.GetClosest(x1, y1, x2, y2);
+      
+        //Assert
+
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsCloser_ShouldReturnSecondPoint()
+    {
+        //Arrange
+        double x1 = 2;
+        double y1 = 2;
+        double x2 = 1;
+        double y2 = 1;
+
+        string expected = "(1, 1)";
+
+        //Act
+        string result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        //Assert
+
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenBothPointsHaveEqualDistance_ShouldReturnFirstPoint()
+    {
+        //Arrange
+        double x1 = 1;
+        double y1 = 2;
+        double x2 = 2;
+        double y2 = 1;
+
+        string expected = "(1, 2)";
+
+        //Act
+        string result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        //Assert
+
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsNegative_ShouldReturnFirstPoint()
+    {
+        //Arrange
+        double x1 = -1;
+        double y1 = 1;
+        double x2 = 1;
+        double y2 = 1;
+
+        string expected = "(-1, 1)";
+
+        //Act
+        string result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        //Assert
+
+        Assert.That(result, Is.EqualTo(expected));
+
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsNegative_ShouldReturnSecondPoint()
+    {
+
+        //Arrange
+        double x1 = 2;
+        double y1 = 2;
+        double x2 = 1;
+        double y2 = -1;
+
+        string expected = "(1, -1)";
+
+        //Act
+        string result = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        //Assert
+
+        Assert.That(result, Is.EqualTo(expected));
+
+
+
+    }
+}
